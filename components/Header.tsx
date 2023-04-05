72810f199a23496003a9cc34008210905aa822c8
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-05 21:19:45 +0900

    4

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,15 @@ import { Navbar } from "flowbite-react";
 
 const Header = () => {
   return (
-      <Navbar>
+    <div className="px-2 sm:px-3 lg:px-10 sm:py-1">
+      <Navbar
+        fluid={true}
+        className="">
         <Navbar.Brand href="/#" className="">
           <CustomImage src="/images/inblog_logo.png" className="h-8 lg:h-9 ml-2 w-auto " />
         </Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse>
-          <div className="py-1">
-          </div>
           <div className="py-1">
             <Navbar.Link href="/contact">
               Contact
@@ -28,6 +29,7 @@ const Header = () => {
           </div>
         </Navbar.Collapse>
       </Navbar>
+      </div>
   );
 };
 
