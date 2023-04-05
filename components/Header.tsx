dc0475777ed7fc5bb73d7ce6edc133dacd9a6945
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-05 21:21:14 +0900

    k

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,12 @@ import { Navbar } from "flowbite-react";
 
 const Header = () => {
   return (
-    <div className="px-2 sm:px-3 lg:px-10 sm:py-1">
+    <div className="px-1 sm:px-3 lg:px-10 sm:py-1">
       <Navbar
         fluid={true}
         className="">
         <Navbar.Brand href="/#" className="">
-          <CustomImage src="/images/inblog_logo.png" className="h-8 lg:h-9 ml-2 w-auto " />
+          <CustomImage src="/images/inblog_logo.png" className="h-7 lg:h-9 ml-2 w-auto " />
         </Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse>
