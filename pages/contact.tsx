95ae07fd981d6be42c2587234d863eebbee979b1
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-05 20:56:15 +0900

    e

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,9 +6,9 @@ const Page = () => {
   return (
     <div>
       <Header />
-      <div className="flex flex-col h-screen items-center bg-[#F4F4F4]">
-        <div className="h-36 lg:h-40" />
-          <div className="font-bold text-3xl lg:text-4xl">
+      <div className="flex flex-col h-screen items-center">
+        <div className="h-16 lg:h-24" />
+          <div className="font-bold text-2xl lg:text-4xl">
             Contact us
           </div>
           <div className="h-4" />
@@ -16,35 +16,29 @@ const Page = () => {
           Please feel free to contact me with any questions you may have.<br></br>
           I will get back to you as soon as possible.
           </div>
-          <div className="sm:hidden text-center font-light md:text-lg">
+          <div className="sm:hidden text-center font-light md:text-lg leading-snug">
           Please feel free to contact me<br></br> with any questions you may have.<br></br>
           I will get back to you as soon as possible.
           </div>
         <div className="h-6" />
-        <div className="w-7/12 lg:w-6/12 bg-[#F4F4F4] px-4 lg:px-16">
+        <div className="w-10/12 lg:w-2/3 xl:w-1/2 px-4 lg:px-16">
           <div className="lg:h-4"></div>
           <div className="flex flex-col w-full">
             <div className="flex flex-col gap-3 lg:flex-row lg:gap-4 w-full">
               <div className="w-full">
-              <span className="mb-2 pl-3 text-4xs lg:text-sm font-medium">
-                Name
-              </span>
-              <div className="w-full rounded-md text-black px-2 py-2 border-black border-solid border-2">
-                <input
-                  className="bg-[#F4F4F4] w-full placeholder:italic placeholder:text-black placeholder:font-light placeholder:opacity-40 border-0 outline-0 font-medium text-5xs px-2 text-black"
-                  placeholder="Full Name"
-                  type="name"
-                  name="name"
-                />
-              </div>
+                <div className="w-full rounded-md text-black px-2 py-2 border-black border-solid border-2">
+                  <input
+                    className="w-full placeholder:italic placeholder:text-black placeholder:font-light border-0 outline-0 font-medium text-5xs px-2 text-black"
+                    placeholder="Full Name"
+                    type="name"
+                    name="name"
+                  />
+                </div>
             </div>
             <div className="w-full">
-              <span className="mb-2 pl-3 text-4xs lg:text-sm font-medium">
-                Email Address
-              </span>
-              <div className="w-full rounded-md text-black px-2 py-2 border-black border-solid border-2">
+              <div className="w-full rounded-md text-black px-2 border-black border-solid border-2">
                 <input
-                  className="bg-[#F4F4F4] w-full placeholder:italic placeholder:text-black placeholder:font-light placeholder:opacity-40 border-0 outline-0 font-medium text-5xs px-2 text-black"
+                  className="w-full placeholder:italic placeholder:text-black placeholder:font-light border-0 outline-0 font-medium text-5xs px-2 text-black"
                   placeholder="Email Address"
                   type="email"
                   name="email"
@@ -53,19 +47,19 @@ const Page = () => {
             </div>
           </div>
           <div className="w-full">
-          <div className="h-6 lg:h-4"></div>
-            <div className="w-full rounded-md text-black px-2 py-2 border-black border-solid border-2">
+          <div className="h-3 lg:h-4"></div>
+            <div className="w-full rounded-md text-black px-2 border-black border-solid border-2">
               <textarea
-                className="bg-[#F4F4F4] w-full h-32 placeholder:italic placeholder:text-black placeholder:font-light placeholder:opacity-40 border-0 outline-0 font-medium text-5xs px-2 text-black"
+                className="w-full h-32 placeholder:italic placeholder:text-black placeholder:font-light border-0 outline-0 font-medium text-5xs px-2 text-black"
                 placeholder="Write your message here"
                 name="message"
               />
             </div>
           </div>
-          <div className="h-8"></div>
+          <div className="h-12"></div>
           <div className="self-center">
             <b
-              className="font-normal text-tiny md:text-base lg:text-lg text-white bg-black px-12 py-2 rounded-md"
+              className="font-normal text-tiny md:text-base lg:text-lg text-white bg-black px-8 py-2 rounded-full tracking-wide"
             >
               Submit
             </b>
