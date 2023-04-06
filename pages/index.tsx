b7aaafdd3d354dbba4ea590fd4efef76c2d74d25
Limseongjoon <sungjun1703@snu.ac.kr>
2023-04-06 22:25:53 +0900

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,17 +11,17 @@ const Page = () => {
         <div className="flex flex-col items-center px-6 lg:px-12 text-black">
           <div className="flex flex-col items-center w-screen">
             <div className="h-16 md:h-36" />
-            <div className="font-bold text-2xl md:text-5xl lg:text-7xl tracking-tight sm:tracking-normal whitespace-nowrap">Build your <span className="text-[#10A42A]">blog in a minute</span></div>
-            <div className="font-bold text-2xl md:text-5xl lg:text-7xl tracking-tight sm:tracking-normal -mt-2 md:-mt-8 whitespace-nowrap">Lead customers to <span className="text-[#ff4b93]">a purchase</span></div>
+            <div className="font-extrabold text-2xl md:text-5xl lg:text-6xl xl:text-7xl -tracking-normal sm:tracking-normal whitespace-nowrap">Build your <span className="text-[#10A42A]">blog in a minute</span></div>
+            <div className="font-extrabold text-2xl md:text-5xl lg:text-6xl xl:text-7xl tracking-tight sm:tracking-normal -mt-2 md:-mt-5 lg:-mt-8 whitespace-nowrap">Lead customers to <span className="text-[#ff4b93]">a purchase</span></div>
             <div className="h-2" />
             <div className="flex sm:hidden text-tiny md:text-xl lg:text-2xl text-center tracking-tight sm:tracking-normal leading-snug sm:leading-none">Provides you SEO-friendly blog infrastructure<br></br>
             with CTA buttons for conversion</div>
-            <div className="hidden sm:flex text-tiny md:text-xl lg:text-2xl text-center tracking-tight sm:tracking-normal leading-snug">Provides you SEO-friendly blog infrastructure<br></br>
+            <div className="hidden font-light sm:flex text-tiny md:text-xl lg:text-2xl text-center tracking-tight sm:tracking-normal leading-snug">Provides you SEO-friendly blog infrastructure<br></br>
             with CTA buttons for conversion</div>
             <div className="h-12 sm:h-14"></div>
             <Link
               href="/#"
-              className="text-tiny md:text-base lg:text-xl font-semibold sm:font-normal text-white bg-black px-8 sm:px-10 py-2 rounded-full"
+              className="text-tiny md:text-base lg:text-xl font-semibold sm:font-normal text-white bg-black px-6 sm:px-8 py-2 rounded-full"
               >
               Create your blog in 1 minute
             </Link>
